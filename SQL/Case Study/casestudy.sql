24d8a448133fc11ed6112e1b20ede0f1c8f3e357
Raghav Govindarajan <raghavgovindarajan123@outlook.com>
2025-03-22 23:18:54 +0530

    Create casestudy.sql

---
+++ SQL/Case Study/casestudy.sql
@@ -0,0 +1,52 @@
+create database payxpert
+
+use payxpert
+
+create table employee
+(
+employeeid int primary key not null,
+firstname varchar(255),
+lastname varchar(255),
+dateofbirth date not null,
+gender varchar(50),
+email varchar(255),
+phonenumber varchar(20) not null,
+address varchar(255),
+position varchar(255),
+joiningdate date not null,
+terminationdate date
+)
+
+create table payroll
+(
+payrollid int primary key not null,
+employeeid int not null,
+constraint fk_payroll_employee foreign key (employeeid) references employee(employeeid),
+payperiodstartdate date not null,
+payperiodenddate date not null,
+basicsalary int not null,
+overtimepay int not null,
+deductions int not null,
+netsalary int not null
+)
+
+create table tax
+(
+taxid int primary key not null,
+employeeid int not null,
+constraint fk_tax_employee foreign key (employeeid) references employee(employeeid),
+taxyear int not null,
+taxableincome int not null,
+taxamount int not null
+)
+
+create table financialrecord
+(
+recordid int primary key not null,
+employeeid int not null,
+constraint fk_financialrecord_employee foreign key (employeeid) references employee(employeeid),
+recorddate date not null,
+description varchar(255),
+amount int not null,
+recordtype varchar(255)
+)
