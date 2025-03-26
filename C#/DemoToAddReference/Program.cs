c66d47b93cd47e727c845aa7e341cc01ad953c76
Raghav Govindarajan <raghavgovindarajan123@outlook.com>
2025-03-26 13:00:31 +0530

    add c# files

---
+++ C#/DemoToAddReference/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoProjectLibrary;
+
+namespace DemoToAddReference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 ObjClass = new Class1();
+            ObjClass.Display();
+        }
+    }
+}
