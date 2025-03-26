05679f924d7dfa905f2dbdedc76e618c26f32637
Raghav Govindarajan <raghavgovindarajan123@outlook.com>
2025-03-26 15:11:43 +0530

    directory structure improvements

---
+++ C#/Assignment/Class Assignment/Namespaces/NamespaceDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Financial;
+using Calculation;
+
+namespace NamespaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Financial.Math mathObject = new Financial.Math();
+            Calculation.Math mathFunc = new Calculation.Math();
+
+            Console.WriteLine(mathFunc);
+            Console.WriteLine(mathObject);
+
+            Financial.Math mathObject1 = new Financial.Math();
+            Calculation.Math mathFunc1 = new Calculation.Math();
+
+        }
+    }
+}
