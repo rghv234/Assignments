b3711f2fd6724eb799a790c2bb7a756e36b3e073
Raghav Govindarajan <raghavgovindarajan123@outlook.com>
2025-03-27 12:33:39 +0530

    variable and dynamic

---
+++ C#/Assignment/Class Assignment/Data Types/VarAndDynamic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarAndDynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var variablevalue = 156.15F;
+
+            Console.WriteLine("Type of variable " + variablevalue.GetType());
+
+            dynamic dynamicvalue = 789;
+
+            Console.WriteLine("Type of dynamic " + dynamicvalue.GetType());
+
+            Console.ReadKey();
+        }
+    }
+}
