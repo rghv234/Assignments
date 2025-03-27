d1f3b4225271cf82107ab877309a52e6bd5155cf
Raghav Govindarajan <raghavgovindarajan123@outlook.com>
2025-03-27 10:45:53 +0530

    float code

---
+++ C#/Assignment/Class Assignment/Data Types/FloatTypeApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatTypeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = 123.15F;
+            double number2 = 5689.26D;
+            decimal number3 = 89325.15m;
+
+            //parsing using convert
+            string numberFloat = Console.ReadLine();
+            number = Convert.ToSingle(numberFloat);
+
+            //parsing using parse method
+            string numberDouble = Console.ReadLine();
+            number2 = float.Parse(numberDouble);
+
+            //parsing using parse method
+            string numberDecimal = Console.ReadLine();
+            number2 = float.Parse(numberDecimal);
+
+            //parsing string
+
+            Console.WriteLine(number);
+            Console.WriteLine(number2); 
+            Console.WriteLine(number3);
+
+            Console.ReadKey();
+        }
+    }
+}
--- C#/Assignment/Class Assignment/Data Types/FloatTypeApp/Properties/AssemblyInfo.cs
+++ C#/Assignment/Class Assignment/Data Types/FloatTypeApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TypesApp")]
+[assembly: AssemblyTitle("FloatTypeApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("TypesApp")]
+[assembly: AssemblyProduct("FloatTypeApp")]
 [assembly: AssemblyCopyright("Copyright © HP 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c34c5cb-c5bd-4c0a-bd82-2252bd55824f")]
+[assembly: Guid("82ff4c59-3b97-42a6-9d94-d4e95600f20d")]
 
 // Version information for an assembly consists of the following four values:
 //
